4ad5d1dc97a72673221eed8c515008917395e953
Elijah Prince <elijahprince73@gmail.com>
2019-06-09 20:13:16 -0700

    small change

--- src/App.js
+++ src/App.js
@@ -1,15 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Routes from './routes';
 
-class App extends Component {
-  componentWillMount() {
-  }
-
-  render() {
-    return (
-      <Routes />
-    );
-  }
-}
+const App = () => <Routes />;
 
 export default App;
